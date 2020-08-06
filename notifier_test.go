762d44c7a9dbda0c0eef9d04175c6dfc3877f75f
Roger Guldbrandsen <roger@kinbiko.com>
2020-08-07 01:38:00 +0900

    [fix] Extract non-race-checker tests to own file
    - Also introduces an extra CI step that executes the race checker.

--- notifier_test.go
+++ notifier_test.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
@@ -101,98 +100,6 @@ func TestMakeExceptions(t *testing.T) {
 			"stacktrace": null
 		}
 	]`)
-}
-
-func TestNoPanicsWhenShuttingDown(t *testing.T) {
-	cfg := Configuration{
-		APIKey:       "abcd1234abcd1234abcd1234abcd1234",
-		ReleaseStage: "dev",
-		AppVersion:   "1.2.3",
-		// Dummy endpoints to avoid accidentally sending payloads to Bugsnag.
-		// Note that the panics are most likely to occur *after* any execution
-		// of the Sanitizer functions, and thus setting different endpoints
-		// that will fail are more appropriate.
-		EndpointNotify:   "http://0.0.0.0:1234",
-		EndpointSessions: "http://0.0.0.0:1234",
-	}
-
-	t.Run("doesn't panic if invoking StartSession or Notify", func(t *testing.T) {
-		n, err := New(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		ctx := n.StartSession(context.Background())
-		n.Notify(ctx, fmt.Errorf("oooi"))
-		n.Close()
-	})
-
-	t.Run("StartSession and Notify are uncalled", func(t *testing.T) {
-		n, err := New(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		n.Close()
-	})
-
-	t.Run("Notify after Close invokes InternalErrorCallback", func(t *testing.T) {
-		var got error
-		cfg.InternalErrorCallback = func(err error) { got = err }
-
-		n, err := New(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		n.Close()
-		n.Notify(context.Background(), fmt.Errorf("oops"))
-
-		if got == nil {
-			t.Fatal("expected error but got none")
-		}
-
-		if exp, got := "did you invoke Notify", got.Error(); !strings.Contains(got, exp) {
-			t.Errorf("expected error message containing %s but got %s", exp, got)
-		}
-	})
-
-	t.Run("StartSession after Close invokes InternalErrorCallback", func(t *testing.T) {
-		var got error
-		cfg.InternalErrorCallback = func(err error) { got = err }
-
-		n, err := New(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		n.Close()
-		n.StartSession(context.Background())
-
-		if got == nil {
-			t.Fatal("expected error but got none")
-		}
-
-		if exp, got := "did you invoke StartSession", got.Error(); !strings.Contains(got, exp) {
-			t.Errorf("expected error message containing %s but got %s", exp, got)
-		}
-	})
-
-	t.Run("Close after Close invokes InternalErrorCallback", func(t *testing.T) {
-		var got error
-		cfg.InternalErrorCallback = func(err error) { got = err }
-
-		n, err := New(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		n.Close()
-		n.Close()
-
-		if got == nil {
-			t.Fatal("expected error but got none")
-		}
-
-		if exp, got := "did you invoke Close", got.Error(); !strings.Contains(got, exp) {
-			t.Errorf("expected error message containing %s but got %s", exp, got)
-		}
-	})
 }
 
 func TestInternalErrorCallback(t *testing.T) {
