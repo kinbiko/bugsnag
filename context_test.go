122c4ba864983dc1b668f818b3ce7bd1c6888871
Roger Guldbrandsen <roger@kinbiko.com>
2020-08-07 02:23:29 +0900

    [chore] Refactor ContextWith single top level test

--- context_test.go
+++ context_test.go
@@ -8,54 +8,55 @@ import (
 	"github.com/kinbiko/jsonassert"
 )
 
-func TestBreadcrumbs(t *testing.T) {
+func TestContextWithMethods(t *testing.T) {
 	n, err := New(Configuration{APIKey: "1234abcd1234abcd1234abcd1234abcd", AppVersion: "1.2.3", ReleaseStage: "test"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	asJSON := func(s interface{}) string {
-		b, err := json.Marshal(s)
-		if err != nil {
-			return "<<ERROR>>"
+	t.Run("WithBreadcrumb", func(t *testing.T) {
+		asJSON := func(s interface{}) string {
+			b, err := json.Marshal(s)
+			if err != nil {
+				return "<<ERROR>>"
+			}
+			return string(b)
 		}
-		return string(b)
-	}
 
-	expLatest := Breadcrumb{Name: "latest"}
-	expNewest := Breadcrumb{Name: "newest", Type: BCTypeLog, Metadata: map[string]interface{}{"md": 1}}
+		expLatest := Breadcrumb{Name: "latest"}
+		expNewest := Breadcrumb{Name: "newest", Type: BCTypeLog, Metadata: map[string]interface{}{"md": 1}}
 
-	ctx := context.Background()
-	ctx = n.WithBreadcrumb(ctx, expLatest)
-	ctx = n.WithBreadcrumb(ctx, expNewest)
+		ctx := n.WithBreadcrumb(context.Background(), expLatest)
+		ctx = n.WithBreadcrumb(ctx, expNewest)
 
-	bcs := makeBreadcrumbs(ctx)
+		bcs := makeBreadcrumbs(ctx)
 
-	if len(bcs) != 2 {
-		t.Fatalf("expected 2 breadcrumbs but got %d", len(bcs))
-	}
+		if len(bcs) != 2 {
+			t.Fatalf("expected 2 breadcrumbs but got %d", len(bcs))
+		}
 
-	ja := jsonassert.New(t)
-	ja.Assertf(asJSON(bcs[0]), `{ "timestamp": "<<PRESENCE>>", "name": "newest", "type": "log", "metaData": { "md": 1 } }`)
-	ja.Assertf(asJSON(bcs[1]), `{ "timestamp": "<<PRESENCE>>", "name": "latest", "type": "manual" }`)
-}
+		ja := jsonassert.New(t)
+		ja.Assertf(asJSON(bcs[0]), `{ "timestamp": "<<PRESENCE>>", "name": "newest", "type": "log", "metaData": { "md": 1 } }`)
+		ja.Assertf(asJSON(bcs[1]), `{ "timestamp": "<<PRESENCE>>", "name": "latest", "type": "manual" }`)
+	})
 
-func TestUserAndContext(t *testing.T) {
-	exp := User{ID: "id", Name: "name", Email: "email"}
-	if got := getAttachedContextData(WithUser(context.Background(), exp)).User; *got != exp {
-		t.Errorf("expected that when I add '%+v' to the context what I get back ('%+v') should be equal", exp, got)
-	}
-}
+	t.Run("WithUser", func(t *testing.T) {
+		exp := User{ID: "id", Name: "name", Email: "email"}
+		if got := getAttachedContextData(WithUser(context.Background(), exp)).User; *got != exp {
+			t.Errorf("expected that when I add '%+v' to the context what I get back ('%+v') should be equal", exp, got)
+		}
+	})
 
-func TestMetadata(t *testing.T) {
-	ctx := WithMetadatum(context.Background(), "app", "id", "15011-2")
-	ctx = WithMetadata(ctx, "device", map[string]interface{}{"model": "15023-2"})
-	md := Metadata(ctx)
-	if appID, exp := md["app"]["id"], "15011-2"; appID != exp {
-		t.Errorf("expected app.id to be '%s' but was '%s'", exp, appID)
-	}
-	if deviceModel, exp := md["device"]["model"], "15023-2"; deviceModel != exp {
-		t.Errorf("expected device.model to be '%s' but was '%s'", exp, deviceModel)
-	}
+	t.Run("WithMetadata and WithMetadatum", func(t *testing.T) {
+		ctx := WithMetadatum(context.Background(), "app", "id", "15011-2")
+		ctx = WithMetadata(ctx, "device", map[string]interface{}{"model": "15023-2"})
+		md := Metadata(ctx)
+		if appID, exp := md["app"]["id"], "15011-2"; appID != exp {
+			t.Errorf("expected app.id to be '%s' but was '%s'", exp, appID)
+		}
+		if deviceModel, exp := md["device"]["model"], "15023-2"; deviceModel != exp {
+			t.Errorf("expected device.model to be '%s' but was '%s'", exp, deviceModel)
+		}
+	})
 }
 
 func TestCtxSerialization(t *testing.T) {
