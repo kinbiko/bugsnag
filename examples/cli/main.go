fbdd6cbfe0fda08974f3c8f8d40c1b8a9d22040b
Roger Guldbrandsen <roger@kinbiko.com>
2020-12-12 21:41:08 +0900

    [docs] Add CLI example
    Also serves as a manual test to verify Close() is reliable.

---
+++ examples/cli/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/kinbiko/bugsnag"
+)
+
+func main() {
+	ctx := context.Background()
+	n, err := bugsnag.New(bugsnag.Configuration{APIKey: os.Getenv("BUGSNAG_API_KEY"), AppVersion: "1.2.3", ReleaseStage: "dev"})
+	if err != nil {
+		panic(err)
+	}
+	defer n.Close()
+
+	n.Notify(n.StartSession(ctx), fmt.Errorf("ooi"))
+
+	// There *may* be a few seconds delay however before it shows up in your dashboard.
+	fmt.Println("application done, closing down immediately, but errors/sessions are still reported.")
+}
