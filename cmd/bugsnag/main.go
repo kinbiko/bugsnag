83e27d77748262e134b79e20790781390de0160c
Roger Guldbrandsen <8797880+kinbiko@users.noreply.github.com>
2022-07-31 19:53:40 +0900

    [feat] Create `bugsnag` cli with basic release reporting (#51)

---
+++ cmd/bugsnag/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const usage = `bugsnag is a tool for calling Bugsnag's APIs
+
+Usage:
+
+bugsnag <command> --flag1 --flag2 (...)
+
+The commands are:
+	release -- Report a build or a release to Bugsnag's build API.
+
+For more details about a command, run:
+
+bugsnag <command> --help`
+
+type application struct {
+	releaseFlags *releaseFlags
+}
+
+func main() {
+	if err := run(os.Args[1:], splitByEquals(os.Environ())); err != nil {
+		fmt.Printf("%s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(args []string, envvars map[string]string) error {
+	releaseCmd := flag.NewFlagSet("release", flag.ExitOnError)
+	app := application{releaseFlags: newReleaseFlags(releaseCmd)}
+
+	if len(args) == 0 {
+		return fmt.Errorf(usage)
+	}
+	switch args[0] {
+	case "release":
+		releaseCmd.Parse(args[1:])
+		return app.runRelease(envvars)
+	}
+	return fmt.Errorf(usage)
+}
